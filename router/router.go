a0ca05888f4493f3fe02b1a9818f0b73c73621ee
y4asse <yasse0218@icloud.com>
2023-07-10 05:08:17 +0900

    delete cors url

--- router/router.go
+++ router/router.go
@@ -11,7 +11,7 @@ import (
 func NewRouter(openaiController controller.IOpenaiController, gameController controller.IGameController) *echo.Echo {
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://localhost:3000", os.Getenv("FRONT_URL"), os.Getenv("FRONT_DEV_URL")},
+		AllowOrigins: []string{os.Getenv("FRONT_URL"), os.Getenv("FRONT_DEV_URL")},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept,
 			echo.HeaderAccessControlAllowHeaders, echo.HeaderXCSRFToken},
 		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE"},
