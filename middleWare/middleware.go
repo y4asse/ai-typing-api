20e4453771f3124199413a7dbbfd9dc96f2dbc91
y4asse <yasse0218@icloud.com>
2023-08-18 23:09:02 +0900

    change to need authorization

---
+++ middleWare/middleware.go
@@ -0,0 +1,81 @@
+package middleWare
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+
+	firebase "firebase.google.com/go"
+	firebaseAuth "firebase.google.com/go/auth"
+	"github.com/labstack/echo/v4"
+	"google.golang.org/api/option"
+)
+
+func Auth() echo.MiddlewareFunc {
+	return auth
+}
+
+func AuthAllowGuest() echo.MiddlewareFunc {
+	return authAllowGuest
+}
+
+func auth(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		opt := option.WithCredentialsFile("firebase_secret_key.json")
+		app, err := firebase.NewApp(context.Background(), nil, opt)
+		if err != nil {
+			fmt.Println(err.Error())
+			return err
+		}
+
+		client, err := app.Auth(context.Background())
+		if err != nil {
+			fmt.Println(err.Error())
+			return err
+		}
+
+		auth := c.Request().Header.Get("Authorization")
+		idToken := strings.Replace(auth, "Bearer ", "", 1)
+		token, err := client.VerifyIDToken(context.Background(), idToken)
+		if err != nil {
+			fmt.Println(err.Error())
+			return c.JSON(http.StatusUnauthorized, err.Error())
+		}
+
+		c.Set("token", token)
+		return next(c)
+	}
+}
+
+func authAllowGuest(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		opt := option.WithCredentialsFile("firebase_secret_key.json")
+		app, err := firebase.NewApp(context.Background(), nil, opt)
+		if err != nil {
+			fmt.Println(err.Error())
+			return err
+		}
+
+		client, err := app.Auth(context.Background())
+		if err != nil {
+			fmt.Println(err.Error())
+			return err
+		}
+
+		auth := c.Request().Header.Get("Authorization")
+		idToken := strings.Replace(auth, "Bearer ", "", 1)
+		if idToken == "undefined" {
+			c.Set("token", &firebaseAuth.Token{})
+			return next(c)
+		}
+		token, err := client.VerifyIDToken(context.Background(), idToken)
+		if err != nil {
+			fmt.Println(err.Error())
+			return c.JSON(http.StatusUnauthorized, err.Error())
+		}
+
+		c.Set("token", token)
+		return next(c)
+	}
+}
--- router/router.go
+++ router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"ai-typing/controller"
+	"ai-typing/middleWare"
 	"os"
 
 	"github.com/labstack/echo/v4"
@@ -12,15 +13,26 @@ func NewRouter(
 	openaiController controller.IOpenaiController,
 	gameController controller.IGameController,
 	createdTextController controller.ICreatedTextController,
-	likeController controller.IlikeController) *echo.Echo {
+	likeController controller.IlikeController,
+	// userController controller.IUerController
+) *echo.Echo {
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{os.Getenv("FRONT_URL"), os.Getenv("FRONT_DEV_URL")},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept,
-			echo.HeaderAccessControlAllowHeaders, echo.HeaderXCSRFToken},
+			echo.HeaderAccessControlAllowHeaders, echo.HeaderXCSRFToken, echo.HeaderAuthorization},
 		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE"},
 		AllowCredentials: true,
 	}))
+	// e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+	// 	CookiePath:     "/",
+	// 	CookieDomain:   os.Getenv("API_DOMAIN"),
+	// 	CookieHTTPOnly: true,
+	// 	CookieSameSite: http.SameSiteDefaultMode,
+	// }))
+
+	//csrf
+	// e.GET("/csrf", userController.CsrfToken)
 
 	//openai
 	e.GET("/aiText", openaiController.GetAiText)
@@ -28,9 +40,7 @@ func NewRouter(
 
 	//game
 	e.GET("/game", gameController.GetAllGame)
-	e.POST("/game", gameController.CreateGame)
 	e.PUT("/gameScore/:id", gameController.UpdateGameScore)
-	e.POST("/gameHistory", gameController.GetGameHistory)
 	e.GET("/gameRanking", gameController.GetGameRanking)
 	e.POST("/latestGames", gameController.GetLatestGames)
 	e.GET("/totalGameCount", gameController.GetTotalGameCount)
@@ -47,5 +57,9 @@ func NewRouter(
 	e.DELETE("/like/:gameId", likeController.Delete)
 	e.GET("likeCountRanking", likeController.GetCountGroupByGameIdOrder)
 	e.GET("likedGameIdCount", likeController.GetGameIdCount)
+
+	//user
+	e.GET("/gameHistory", gameController.GetGameHistory, middleWare.Auth())
+	e.POST("/game", gameController.CreateGame, middleWare.AuthAllowGuest())
 	return e
 }
